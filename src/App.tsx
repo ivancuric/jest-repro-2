5461ad765f540a7d77aee4dd5e09ff10cc1a8e6f
Ivan Čurić <ivan.curic@gmail.com>
2021-03-26 17:32:41 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+import create, { SetState } from "zustand";
+
+type AppState = {
+  count: number;
+};
+
+type Actions = {
+  add: (number: number) => void;
+  reset: () => void;
+};
+
+type Store = AppState & Actions;
+
+const initialState: AppState = {
+  count: 0,
+};
+
+const createActions = (set: SetState<Store>) =>
+  ({
+    add: (number) => set((state) => ({ count: state.count + number })),
+    reset: () => set({ count: 0 }),
+  } as Actions);
+
+export const useStore = create<Store>((set) => ({
+  ...initialState,
+  ...createActions(set),
+}));
+
+export const App = () => {
+  // trigger `act` errors
+  const add = useStore((state) => state.add);
+  useEffect(() => {
+    add(0);
+  }, [add]);
+
+  return (
+    <div>
+      <Counter />
+      <Controls />
+    </div>
+  );
+};
+
+const Counter = () => {
+  const count = useStore((state) => state.count);
+
+  return (
+    <strong style={{ fontSize: "40px" }} data-testid="count">
+      {count}
+    </strong>
+  );
+};
+
+const Controls = () => {
+  const add = useStore((state) => state.add);
+  const reset = useStore((state) => state.reset);
+
+  return (
+    <div>
+      <button onClick={() => add(1)}>Add 1</button>
+      <button onClick={() => reset()}>Reset</button>
+    </div>
+  );
+};
